846c37d0e9b40d7cb6ed9983a73635422ef9e650
Sam Cole <scole3@uic.edu>
2019-03-03 09:53:19 -0600

    Add function to compute approximate permutation similarity (nu)
    If A and B are permutation similar, then nu(A, B) should = n.

---
+++ approxPermSim.m
@@ -0,0 +1,33 @@
+function nu = approxPermSim(A, B)
+  n = size(A, 1);
+
+  U = kron(ones(n, 1), eye(n));
+  V = kron(eye(n), ones(n, 1));
+
+  u1 = U(:, 1);
+
+  C = [U.' - ones(n, 1) * u1.'; V.' - ones(n, 1) * u1.'];
+
+  C = [C(2:end, :); kron(B.', eye(n)) - kron(eye(n), A)]; % C is (2n + n^2) x n^2
+
+  W = null(C); % TODO: what if C has full (column) rank?  Then W is empty...
+  k = size(W, 2);
+
+  [Q, R] = qr(W);
+
+  E = [zeros(k, n^2 -k), eye(k)];
+
+  cvx_begin
+
+  variable Z(n^2, n^2) semidefinite nonnegative
+
+  maximize(trace(Z))
+
+  subject to
+  E * Q.' * Z * W == zeros(k, k)
+  ones(1, n^2) * Z * ones(n^2, 1) == n^2
+  
+  cvx_end
+  
+  nu = cvx_optval;
+end
