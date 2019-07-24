4bdc0922d70851cfff7da60291ac25cfacea680e
Sam Cole <smpcole@gmail.com>
2019-07-23 19:57:53 -0500

    Add lp() function to iteratively compute indices which must be 0 or 1

---
+++ lp.m
@@ -0,0 +1,28 @@
+function [L, U] = lp(A, B)
+  n = size(A, 1);
+  L = zeros(n);
+  U = ones(n);
+
+  Lprev = L;
+  Uprev = U;
+
+  TOL = 10^-9;
+
+  k = 0;
+  
+  while true
+    [L, U] = lpIter(A, B, Lprev, Uprev);
+
+    k = k + 1;
+    
+    if L(1, 1) == -Inf || U(1, 1) == Inf || all(all(abs([L - Lprev, U - Uprev]) < TOL))
+      fprintf('Terminated after %d iteration(s)', k);
+      break;
+    end
+
+    Lprev = L;
+    Uprev = U;
+    
+  end
+  
+end
