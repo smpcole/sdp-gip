7e992e809ad4461914d67b124ab9a396b86db132
Sam Cole <smpcole@gmail.com>
2020-02-19 11:18:12 -0600

    Add SDPNAL+ intallation
    WARNING: CVX doesn't work after running SDPNAL+

--- startup.m
+++ startup.m
@@ -1,3 +1,16 @@
+%%***********************************************************************
+%%
+%%
+%%***********************************************************************
+
+  restoredefaultpath;
+  addpath(genpath(pwd),path);  
+%%
+  set(0,'defaultaxesfontsize',14);
+  set(0,'defaultlinemarkersize',6);
+  set(0,'defaulttextfontsize',14);
+%%***********************************************************************
+
 run /Users/samcole/Documents/MATLAB/startup.m
 
 A = shrikhande(4);
