12b4c9779844842dddeeaeb0aaf070cca56a6082
Sam Cole <samuel.cole@umanitoba.ca>
2020-05-10 15:30:30 -0400

    Run CVX startup script from local startup.m

--- startup.m
+++ startup.m
@@ -10,7 +10,7 @@
   set(0,'defaultlinemarkersize',6);
   set(0,'defaulttextfontsize',14);
 %%***********************************************************************
-
+run /home/smpcole/.matlab/cvx/cvx_startup.m
 run(sprintf('%s/startup.m', userpath))
 
 A = shrikhande(4);
