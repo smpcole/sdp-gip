537cddba6e61826eee01aaa95b8c3d912ff2761a
Sam Cole <smpcole@gmail.com>
2021-11-22 13:29:54 -0600

    Update startup script

--- startup.m
+++ startup.m
@@ -1,17 +1,4 @@
-%%***********************************************************************
-%%
-%%
-%%***********************************************************************
-
-  restoredefaultpath;
-  addpath(genpath(pwd),path);  
-%%
-  set(0,'defaultaxesfontsize',14);
-  set(0,'defaultlinemarkersize',6);
-  set(0,'defaulttextfontsize',14);
-%%***********************************************************************
-run /home/smpcole/.matlab/cvx/cvx_startup.m
-run(sprintf('%s/startup.m', userpath))
+run /home/sam/.matlab/cvx/cvx_startup.m
 
 A = shrikhande(4);
 B = lineGraph(completeBipartite(4, 4));
